b6a8706e05c9d48ad8ce3e393a2a9a08fb5e4915
root <root@vultr.guest>
2018-06-06 02:57:48 +0000

    add start and stop

---
+++ start.sh
@@ -0,0 +1,6 @@
+screen -S server -X quit
+screen -dmS server
+sleep 2s
+cmd=$"node ./src/server.js"
+screen -S server -X stuff "$cmd"
+screen -S server -X stuff $'\n'
---
+++ stop.sh
@@ -0,0 +1,1 @@
+screen -S server -X quit
